bbafa11e7471c578b342eec20b1c4ce8a6ec501b
ken-morel <engonken8@gmail.com>
2026-01-20 19:40:11 +0100

    updated gama toolchain to enable autosizing and web build support

--- include/gama/compress.h
+++ include/gama/compress.h
@@ -16,7 +16,7 @@ void gm_compressed_free(gmCompressed *compressed) {
   memset(compressed, 0, sizeof(*compressed));
 }
 
-int gm_compress_to(const char *data, int data_len, char *dest,
+int gm_compress_to(const unsigned char *data, size_t data_len, unsigned char *dest,
                    size_t dest_len) {
   return lzav_compress_hi(data, dest, data_len, dest_len);
 }
@@ -24,10 +24,10 @@ size_t gm_compressed_size_guess(size_t data_size) {
   return lzav_compress_bound_hi(data_size);
 }
 
-gmCompressed *gm_compress(const char *data, size_t data_len) {
+gmCompressed *gm_compress(const unsigned char *data, size_t data_len) {
   gmCompressed *c = calloc(1, sizeof(gmCompressed));
   size_t max_len = gm_compressed_size_guess(data_len);
-  char *dest = malloc(max_len);
+  unsigned char *dest = malloc(max_len);
   if (!dest)
     return NULL;
   size_t used = gm_compress_to(data, data_len, dest, max_len);
@@ -35,7 +35,7 @@ gmCompressed *gm_compress(const char *data, size_t data_len) {
     free(dest);
     return NULL;
   }
-  char *sized = realloc(dest, used);
+  unsigned char *sized = realloc(dest, used);
   c->compressed = used;
   c->original = data_len;
   c->data = !sized ? dest : sized;
